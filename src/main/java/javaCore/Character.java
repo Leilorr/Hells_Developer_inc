07a744a3cb7f49f9f9bf626a8a6e0d97e9b23bfb
Leilorr <148541216+Leilorr@users.noreply.github.com>
2024-05-23 22:41:54 +0300

    Абстракция, абстракция!

---
+++ src/main/java/javaCore/Character.java
@@ -0,0 +1,23 @@
+package javaCore;
+
+import lombok.Data;
+
+@Data
+public abstract class Character {
+    private String name;
+    protected int strength;
+    protected int agility;
+    protected int intellect;
+    protected int health = 100;
+
+    public Character(String name, int strength, int agility, int intellect) {
+        this.name = name;
+        this.strength = strength;
+        this.agility = agility;
+        this.intellect = intellect;
+    }
+    public Character(String name){
+        this.name = name;
+    }
+    public abstract void attack(Character target);
+}
