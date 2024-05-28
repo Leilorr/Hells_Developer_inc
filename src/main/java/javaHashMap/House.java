f001d58749a0946c04f6b3f3c2b1656c2bde669a
Leilorr <148541216+Leilorr@users.noreply.github.com>
2024-05-28 23:26:17 +0300

    Игра престолов

---
+++ src/main/java/javaHashMap/House.java
@@ -0,0 +1,54 @@
+package javaHashMap;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+@Data
+@AllArgsConstructor
+public class House {
+    private String name;
+    private String sigil;
+    }
+
+    class Main{
+    public static void main(String[] args) {
+        Map<String, House> map = new HashMap<>();
+        map.put("Stark", new House("Stark", "Direwolf"));
+        map.put("Lannister", new House("Lannister", "Lion rampant"));
+        map.put("Barateon", new House("Barateon", "Blak deer"));
+        map.put("Tarli", new House("Tarli", "Red hunter"));
+
+        addNewHouse(map,"Tully", "Fish");
+        System.out.println(map);
+        deliteHouse(map, "Lannister");
+        System.out.println(map);
+        System.out.println(findHouseSigil(map,"Tully"));
+        printAllHouses(map);
+    }
+
+    public static Map<String, House> addNewHouse (Map<String, House> map, String name, String sigil) {
+        map.put(name, new House(name, sigil));
+        return map;
+    }
+
+    public static Map<String, House> deliteHouse (Map<String, House> map, String name){
+        map.remove(name);
+        return map;
+    }
+
+    public static String findHouseSigil(Map<String, House> map, String name){
+        if (map.containsKey(name)) {
+            return map.get(name).getSigil();
+        } else {
+            return "House not found";
+        }
+    }
+
+    public static void printAllHouses(Map<String, House> map){
+        for (Map.Entry<String, House> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue().getSigil());
+        }
+    }
+}
