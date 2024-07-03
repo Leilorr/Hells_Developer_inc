66c4bedc1b136fb95a37e7ce06c3799859088333
Leilorr <148541216+Leilorr@users.noreply.github.com>
2024-07-03 17:17:55 +0300

    Библиотека Вестероса

--- src/main/java/javaHashMap/Book.java
+++ src/main/java/javaHashMap/Book.java
@@ -13,20 +13,8 @@ public class Book {
     private String title;
     private String author;
     private int year;
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, author, year);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass()!= obj.getClass()) return false;
-        Book book = (Book) obj;
-        return Objects.equals(title, book.title) && Objects.equals(author, book.author) && year == book.year;
-    }
 }
+
 class Main {
     private static Map<Book, String> books = new HashMap<>();
 
@@ -47,15 +35,16 @@ class Main {
         infoBooks();
     }
 
-    public static Map<Book, String> addBook(Book book, String place){
-        books.put(book, place);
+    public static Map<Book, String> addBook(Book book, String place) {
+        if (book != null && place != null) {
+            books.put(book, place);
+        }
         return books;
     }
 
     public static String deleteBook(String title, String author, int year) {
-        Book book = new Book(title, author, year);
-        if (books.containsKey(book)) {
-            books.remove(book);
+        if (books.containsKey(newBook(title, author, year))) {
+            books.remove(newBook(title, author, year));
             return "Книга удалена";
         } else {
             return "Ошибка удаления";
@@ -63,9 +52,7 @@ class Main {
     }
 
     public static void searchBook(String title, String author, int year) {
-        Book searchBook = new Book(title, author, year);
-        String location = books.get(searchBook);
-        System.out.println("Местонахождение книги: " + location);
+        System.out.println("Местонахождение книги: " + books.get(newBook(title, author, year)));
     }
 
     public static void infoBooks() {
@@ -73,4 +60,10 @@ class Main {
             System.out.println(entry.getKey().getTitle() + " - Местонахождение: " + entry.getValue());
         }
     }
+
+    public static Book newBook(String title, String author, int year) {
+        Book book = new Book(title, author, year);
+        return book;
+    }
+
 }
