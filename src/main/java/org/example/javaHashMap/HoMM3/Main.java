fc25ca61fa66f7bf61eb036621a9da8e612a4df1
Leilorr <148541216+Leilorr@users.noreply.github.com>
2025-02-19 10:08:42 +0300

    Условия задачи Разработайте систему управления армией в сеттинге игры Heroes of Might and Magic III, используя основы ООП и коллекции. Ваша задача — создать иерархию классов для разных видов существ и героев, а также управление армией героя.
    Требования:
    
    Создайте абстрактный класс Creature с основными характеристиками, такими как имя, уровень, атака, защита, скорость и количество. Также добавьте метод getDamage(), который будет возвращать урон, наносимый существом.
    
    Создайте несколько подклассов, таких как Pikeman, Griffin, Swordman, Angel, каждый из которых наследует от класса Creature. Установите соответствующие значения характеристик для каждого подкласса.
    
    Создайте класс Hero с основными характеристиками, такими как имя, фракция, опыт и уровень. В этом классе реализуйте следующие методы:addCreature(Creature creature, int quantity) — добавить существ в армию героя; removeCreature(Creature creature, int quantity) — удалить существ из армии героя; getArmy() — получить список всех существ в армии героя.
    
    Создайте класс Battlefield, который будет содержать двух героев и их армии. В этом классе реализуйте метод battle(), который будет имитировать сражение между двумя армиями и возвращать победившего героя.
    
    Напишите тестовый код для проверки работы вашей системы управления армией в сеттинге Heroes of Might and Magic III. Создайте несколько героев и существ, добавьте их в армию, а затем проведите сражение между армиями.
    
    Почему эта задача полезна?
    Эта задача предоставляет возможность практического применения основ объектно-ориентированного программирования (ООП) и работы с коллекциями. Работа в контексте игрового программирования помогает разработчику развить навыки моделирования игровых ситуаций, эффективного управления персонажами и создания интерактивных систем.

--- src/main/java/org/example/javaHashMap/HoMM3/Main.java
+++ src/main/java/org/example/javaHashMap/HoMM3/Main.java
@@ -8,19 +8,16 @@ public class Main {
         Hero heroOne = new Hero("Orrin", "Castle", 100, 5,new ArrayList<>());
         Hero heroTwo = new Hero("Crag Hack", "Stronghold", 120, 6, new ArrayList<>());
 
-        // Создаем существ
         Creature pikeman = new Pikeman(50);
         Creature griffin = new Griffin(20);
         Creature swordsman = new Swordman(30);
         Creature angel = new Angel(5);
 
-        // Добавляем существ в армии героев
         heroOne.addCreatures(pikeman, 50);
         heroOne.addCreatures(swordsman, 30);
         heroTwo.addCreatures(griffin, 20);
         heroTwo.addCreatures(angel, 5);
 
-        // Проводим сражение
         Battlefield battlefield = new Battlefield(heroOne, heroTwo);
         battlefield.battle();
     }
